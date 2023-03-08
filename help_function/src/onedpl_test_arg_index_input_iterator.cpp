b12242d041bd85ccb23afe9dfa90076cdeac73cb
Dan Hoeflinger <dan.hoeflinger@intel.com>
2023-03-08 08:47:36 -0500

    fixing comment formatting
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- help_function/src/onedpl_test_arg_index_input_iterator.cpp
+++ help_function/src/onedpl_test_arg_index_input_iterator.cpp
@@ -1,12 +1,11 @@
-// ====------ onedpl_test_arg_index_input_iterator.cpp---------- -*- C++ -*
-// ----===////
+// ====--- onedpl_test_arg_index_input_iterator.cpp------ -*- C++ -* ----===////
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //
-// ===----------------------------------------------------------------------===//
+// ===---------------------------------------------------------------------===//
 
 #include <oneapi/dpl/algorithm>
 #include <oneapi/dpl/execution>
